dc9c634a48cdc328ce71473a76a7f909c1428244
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-08-19 18:31:39 +0100

    basic types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
-let a = 5;
-console.log(a);
+// Basic types
+let n = 5;
+let s = "Hello";
+let b = true;
+let a = undefined; // any type
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-let a : number = 5;
-console.log(a)
+// Basic types
+let n: number = 5;
+let s: string = "Hello";
+let b: boolean = true;
+
+let a: any = undefined; // any type
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [ 1, true, "Hello"]
